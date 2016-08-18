51508b45ae612ca057eeaec449e7fc19e0c36429
AfroMonkey <moisalejandro@gmail.com>
2016-08-18 13:55:15 -0500

    [IMP] swap function

--- quick_sort.hpp
+++ quick_sort.hpp
@@ -1,1 +1,22 @@
- 
+#ifndef QUICK_SORT_HPP
+#define QUICK_SORT_HPP
+
+#include <cstddef>
+#include <iostream>
+#include <cstring>
+
+void swap(void* a, void* b, size_t size)
+{
+    char *c = (char*)malloc(size);
+    std::memcpy(c,a,size);
+    std::memcpy(a,b,size);
+    std::memcpy(b,c,size);
+    free(c);
+}
+
+void qsort(void* base, size_t num, size_t size, int((*compar)(const void*, const void*)))
+{
+    //TODO
+}
+
+#endif
