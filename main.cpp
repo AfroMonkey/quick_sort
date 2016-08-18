549b8a40d027727a2c6056de201056fa9d252af2
AfroMonkey <moisalejandro@gmail.com>
2016-08-18 13:55:25 -0500

    [IMP] main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
- 
+#include "quick_sort.hpp"
+
+int compare(const void* a, const void* b)
+{
+    return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    int array[] = {1, 5, 12, 7, 90, 1212, 43, 12, 8, 1, 2, 34, 6, 7};
+    qsort(array, 14, sizeof(int), compare);
+    for (int i = 0; i < 14; i++)
+    {
+        std::cout << array[i] << std::endl;
+    }
+    return 0;
+}
